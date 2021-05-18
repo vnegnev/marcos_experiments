04e41475348b23efef2ca1fa189d9b64b3d2c1df
Vlad Negnevitsky <vee neg nev at gee mail dot com>
2021-05-19 00:34:06 +0200

    Add my first experiment

---
+++ my_first_experiment.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+st = pdb.set_trace
+
+import external # imports external.py
+import experiment as ex
+
+def my_first_experiment():
+    # Write experiment here!
+
+if __name__ == "__main__":
+    my_first_experiment()
